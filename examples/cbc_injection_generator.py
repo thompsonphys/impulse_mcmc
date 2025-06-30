539e13f09d1e13a9f85d323f836090b3e93aa64a
Jonathan Thompson <thompson.phys@gmail.com>
2025-06-30 13:19:09 +0100

    pushing changes

--- examples/cbc_injection_generator.py
+++ examples/cbc_injection_generator.py
@@ -7,6 +7,7 @@ import pycbc.pnutils as utils
 from pycbc.conversions import mchirp_from_mass1_mass2
 import configparser
 
+
 class CBCInjectionGenerator:
     def __init__(self, config_path):
         """Generate an injection class for storing useful things
@@ -22,16 +23,18 @@ class CBCInjectionGenerator:
         # initialize injection parameters
         self.parameter_dict = _get_parameters_from_config_file(config_path)
 
-        self.injection_options = self.parameter_dict['injection']
-        self.cbc_parameters = self.parameter_dict['parameters']
-        self.recovery_options = self.parameter_dict['recovery']
+        self.injection_options = self.parameter_dict["injection"]
+        self.cbc_parameters = self.parameter_dict["parameters"]
+        self.recovery_options = self.parameter_dict["recovery"]
 
         # Calculate candidate sampling frequencies of hyperprior corners
         self.sampling_frequency = get_sampling_frequency(self.cbc_parameters)
         # Calculate candidate durations of hyperprior corners
-        self.duration = get_duration(self.cbc_parameters, self.injection_options['f_low'])
+        self.duration = get_duration(
+            self.cbc_parameters, self.injection_options["f_low"]
+        )
 
-        self.initialize_detector_network(self.injection_options['detector'])
+        self.initialize_detector_network(self.injection_options["detector"])
         self.generate_injection()
         self.initialize_waveform_generator()
 
@@ -47,72 +50,88 @@ class CBCInjectionGenerator:
 
         if isinstance(interferometer, list):
             ifo_list = [
-                bilby.gw.detector.load_interferometer((inter)) for inter in interferometer]
+                bilby.gw.detector.load_interferometer((inter))
+                for inter in interferometer
+            ]
         else:
-            ifo_list = [
-                bilby.gw.detector.load_interferometer((interferometer))]
-        
+            ifo_list = [bilby.gw.detector.load_interferometer((interferometer))]
+
         self.ifo_list = ifo_list
         self.ifos = bilby.gw.detector.InterferometerList(ifo_list)
 
     def generate_injection(self):
-        """Generate signal injection in interferometer
-
-        """
+        """Generate signal injection in interferometer"""
         self.injection_arguments = dict(
-            reference_frequency=self.injection_options['f_ref'],
-            minimum_frequency=self.injection_options['f_low'], 
+            reference_frequency=self.injection_options["f_ref"],
+            minimum_frequency=self.injection_options["f_low"],
         )
-        if self.injection_options['approximant'] == "NR_hdf5":
-            self.injection_arguments["waveform_approximant"] = self.injection_options['approximant']
-            self.injection_arguments["numerical_relativity_file"] = self.injection_options['numrel_path']
-        elif self.injection_options['approximant'] == "NRHybSur3dq8_22":
+        if self.injection_options["approximant"] == "NR_hdf5":
+            self.injection_arguments["waveform_approximant"] = self.injection_options[
+                "approximant"
+            ]
+            self.injection_arguments["numerical_relativity_file"] = (
+                self.injection_options["numrel_path"]
+            )
+        elif self.injection_options["approximant"] == "NRHybSur3dq8_22":
             self.injection_arguments["waveform_approximant"] = "NRHybSur3dq8"
-            self.injection_arguments["mode_array"] = [[2,2], [2,-2]]
+            self.injection_arguments["mode_array"] = [[2, 2], [2, -2]]
         else:
-            self.injection_arguments["waveform_approximant"] = self.injection_options['approximant']
+            self.injection_arguments["waveform_approximant"] = self.injection_options[
+                "approximant"
+            ]
 
         self.injection_generator = bilby.gw.WaveformGenerator(
-            duration=self.duration, sampling_frequency=self.sampling_frequency,
+            duration=self.duration,
+            sampling_frequency=self.sampling_frequency,
             frequency_domain_source_model=bilby.gw.source.lal_binary_black_hole,
             parameter_conversion=bilby.gw.conversion.convert_to_lal_binary_black_hole_parameters,
-            waveform_arguments=self.injection_arguments)
+            waveform_arguments=self.injection_arguments,
+        )
         self.injection_parameters = self.cbc_parameters.copy()
-        self.injection_parameters.update({
-            'luminosity_distance': 64.0
-        })
+        self.injection_parameters.update({"luminosity_distance": 64.0})
 
         self.ifos.set_strain_data_from_zero_noise(
-            sampling_frequency=self.sampling_frequency, duration=self.duration,
-            start_time=self.injection_parameters['geocent_time'] - (self.duration - 2.0)
+            sampling_frequency=self.sampling_frequency,
+            duration=self.duration,
+            start_time=self.injection_parameters["geocent_time"]
+            - (self.duration - 2.0),
         )
         self.ifos.inject_signal(
-            waveform_generator=self.injection_generator, parameters=self.injection_parameters)
-        
+            waveform_generator=self.injection_generator,
+            parameters=self.injection_parameters,
+        )
+
         # Calculate correct luminosity distance to achieve targeted network SNR
-        candidate_network_snr = \
-            np.sqrt(np.sum([ifo.meta_data['optimal_SNR']**2 for ifo in self.ifos]))
-        self.injection_parameters['luminosity_distance'] = \
-            candidate_network_snr \
-            * self.injection_parameters['luminosity_distance'] \
-            / self.injection_options['snr']
+        candidate_network_snr = np.sqrt(
+            np.sum([ifo.meta_data["optimal_SNR"] ** 2 for ifo in self.ifos])
+        )
+        self.injection_parameters["luminosity_distance"] = (
+            candidate_network_snr
+            * self.injection_parameters["luminosity_distance"]
+            / self.injection_options["snr"]
+        )
 
         # Set up network and inject waveform at target network snr
         self.ifos.set_strain_data_from_zero_noise(
-            sampling_frequency=self.sampling_frequency, duration=self.duration,
-            start_time=self.injection_parameters['geocent_time'] - (self.duration - 2.0)
+            sampling_frequency=self.sampling_frequency,
+            duration=self.duration,
+            start_time=self.injection_parameters["geocent_time"]
+            - (self.duration - 2.0),
         )
         self.ifos.inject_signal(
-            waveform_generator=self.injection_generator, parameters=self.injection_parameters)
-        
-    def initialize_waveform_generator(self):
-        """Generate recovery waveform generator
+            waveform_generator=self.injection_generator,
+            parameters=self.injection_parameters,
+        )
 
-        """
+    def initialize_waveform_generator(self):
+        """Generate recovery waveform generator"""
         self.waveform_generator = get_waveform_generator(
-            self.recovery_options['approximant'], 
-            self.sampling_frequency, self.duration,
-            self.recovery_options['f_low'], self.recovery_options['f_ref'])
+            self.recovery_options["approximant"],
+            self.sampling_frequency,
+            self.duration,
+            self.recovery_options["f_low"],
+            self.recovery_options["f_ref"],
+        )
 
 
 def _get_uniform_chirp_mass_q_prior(mass_1, mass_2):
@@ -128,8 +147,8 @@ def _get_uniform_chirp_mass_q_prior(mass_1, mass_2):
     """
     q = mass_1 / mass_2
     chirp_mass = mchirp_from_mass1_mass2(mass_1, mass_2)
-    q_prior = np.r_[1/(q*1.15), np.min([1/(q*0.85), 1.0])]
-    chirp_mass_prior = [chirp_mass*0.85, chirp_mass*1.15]
+    q_prior = np.r_[1 / (q * 1.15), np.min([1 / (q * 0.85), 1.0])]
+    chirp_mass_prior = [chirp_mass * 0.85, chirp_mass * 1.15]
     return q_prior, chirp_mass_prior
 
 
@@ -146,22 +165,28 @@ def _get_parameters_from_config_file(config_file):
     config = configparser.ConfigParser()
     config.read(config_file)
 
-    config_dict = {k:dict(config.items(k)) for k in config.sections()}
+    config_dict = {k: dict(config.items(k)) for k in config.sections()}
 
-    injection_parameters = config_dict['injection'].copy()
-    cbc_parameters = config_dict['parameters'].copy()
-    recovery_parameters = config_dict['recovery'].copy()
+    injection_parameters = config_dict["injection"].copy()
+    cbc_parameters = config_dict["parameters"].copy()
+    recovery_parameters = config_dict["recovery"].copy()
 
     # convert floats appropriately
-    injection_parameters.update({k:float(injection_parameters[k]) for k in ['f_low', 'f_ref', 'snr']})
-    cbc_parameters = {a:float(b) for a,b in cbc_parameters.items()}
-    recovery_parameters.update({k:float(recovery_parameters[k]) for k in ['f_low', 'f_ref']})
-    recovery_parameters.update({k:int(recovery_parameters[k]) for k in ['ntemps', 'nsamples']})
+    injection_parameters.update(
+        {k: float(injection_parameters[k]) for k in ["f_low", "f_ref", "snr"]}
+    )
+    cbc_parameters = {a: float(b) for a, b in cbc_parameters.items()}
+    recovery_parameters.update(
+        {k: float(recovery_parameters[k]) for k in ["f_low", "f_ref"]}
+    )
+    recovery_parameters.update(
+        {k: int(recovery_parameters[k]) for k in ["ntemps", "nsamples"]}
+    )
 
     return {
-        'injection':injection_parameters,
-        'parameters':cbc_parameters,
-        'recovery':recovery_parameters
+        "injection": injection_parameters,
+        "parameters": cbc_parameters,
+        "recovery": recovery_parameters,
     }
 
 
@@ -176,27 +201,28 @@ def _get_hyper_prior(parameter_dict):
     # convert parameters
     # Create hyperprior to choose appropriate PE sample rate and seglen
 
-    mass_1 = parameter_dict['mass_1']
-    mass_2 = parameter_dict['mass_2']
-    chi_1 = parameter_dict['chi_1']
-    chi_2 = parameter_dict['chi_2']
+    mass_1 = parameter_dict["mass_1"]
+    mass_2 = parameter_dict["mass_2"]
+    chi_1 = parameter_dict["chi_1"]
+    chi_2 = parameter_dict["chi_2"]
 
     chi_1_prior = np.r_[-0.99, 0.99]
     chi_2_prior = np.r_[-0.99, 0.99]
     q_prior, chirp_mass_prior = _get_uniform_chirp_mass_q_prior(mass_1, mass_2)
     chirp_mass_q_prior = np.array(np.meshgrid(chirp_mass_prior, q_prior))
     chirp_mass_q_prior = chirp_mass_q_prior.T.reshape(-1, 2)
-    mass_1_list, mass_2_list = utils.mchirp_q_to_mass1_mass2(
-        *chirp_mass_q_prior.T)
-    hyper_prior = np.array([
-        [_mass_1, _mass_2, _chi_1, _chi_2]
-        for (_mass_1, _mass_2) in zip(mass_1_list, mass_2_list)
-        for _chi_1 in chi_1_prior
-        for _chi_2 in chi_2_prior])
+    mass_1_list, mass_2_list = utils.mchirp_q_to_mass1_mass2(*chirp_mass_q_prior.T)
+    hyper_prior = np.array(
+        [
+            [_mass_1, _mass_2, _chi_1, _chi_2]
+            for (_mass_1, _mass_2) in zip(mass_1_list, mass_2_list)
+            for _chi_1 in chi_1_prior
+            for _chi_2 in chi_2_prior
+        ]
+    )
     hyper_prior = np.append(
-        hyper_prior,
-        np.array([[mass_1, mass_2, chi_1, chi_2]]),
-        axis=0)
+        hyper_prior, np.array([[mass_1, mass_2, chi_1, chi_2]]), axis=0
+    )
     return hyper_prior
 
 
@@ -211,8 +237,9 @@ def get_sampling_frequency(parameter_dict):
     hyper_prior = _get_hyper_prior(parameter_dict)
     # Calculate candidate sampling frequencies of hyperprior corners
     sampling_frequencies = utils.frequency_cutoff_from_name(
-        'SEOBNRv4RD', *hyper_prior.T)
-    return ceilpow2(sampling_frequencies.max())*2
+        "SEOBNRv4RD", *hyper_prior.T
+    )
+    return ceilpow2(sampling_frequencies.max()) * 2
 
 
 def get_duration(parameter_dict, f_low):
@@ -227,8 +254,8 @@ def get_duration(parameter_dict, f_low):
     """
     hyper_prior = _get_hyper_prior(parameter_dict)
     durations = utils.get_imr_duration(
-        *hyper_prior.T, f_low=f_low,
-        approximant='IMRPhenomD')
+        *hyper_prior.T, f_low=f_low, approximant="IMRPhenomD"
+    )
     return ceilpow2(durations.max())
 
 
@@ -251,10 +278,14 @@ def get_waveform_generator(approximant, sampling_frequency, duration, f_low, f_r
     # Prepare model waveform generator
     waveform_arguments = dict(
         waveform_approximant=approximant,
-        reference_frequency=f_ref, minimum_frequency=f_low)
+        reference_frequency=f_ref,
+        minimum_frequency=f_low,
+    )
     waveform_generator = bilby.gw.WaveformGenerator(
-        duration=duration, sampling_frequency=sampling_frequency,
+        duration=duration,
+        sampling_frequency=sampling_frequency,
         frequency_domain_source_model=bilby.gw.source.lal_binary_black_hole,
         parameter_conversion=bilby.gw.conversion.convert_to_lal_binary_black_hole_parameters,
-        waveform_arguments=waveform_arguments)
+        waveform_arguments=waveform_arguments,
+    )
     return waveform_generator
--- examples/test_model_wvfm.py
+++ examples/test_model_wvfm.py
@@ -1,8 +1,12 @@
 import numpy as np
 from cbc_injection_generator import CBCInjectionGenerator
+from scipy.integrate import simpson
+from pycbc.waveform.utils import ceilpow2
+import pyfftw
+
 
 # standard zero-noise matched-filter likelihood
-class LnLikelihoodWvfm():
+class LnLikelihoodWvfm:
     def __init__(self, config_file):
         """class init
 
@@ -20,13 +24,19 @@ class LnLikelihoodWvfm():
         self.frequency_mask = self.ifo.frequency_mask
         self.psd = self.ifo.power_spectral_density_array[self.frequency_mask]
 
-        self.signal_polarizations = self.injection.injection_generator.frequency_domain_strain(self.injection.injection_parameters)
-        self.signal_response = self.ifo.get_detector_response(self.signal_polarizations, self.injection.injection_parameters)[self.frequency_mask]
+        self.signal_polarizations = (
+            self.injection.injection_generator.frequency_domain_strain(
+                self.injection.injection_parameters
+            )
+        )
+        self.signal_response = self.ifo.get_detector_response(
+            self.signal_polarizations, self.injection.injection_parameters
+        )[self.frequency_mask]
 
-        self.delta_f = self.ifo.frequency_array[1] - self.ifo.frequency_array[0]
+        self.f = self.ifo.frequency_array[self.frequency_mask]
 
-        self.ntemps = self.injection.recovery_options['ntemps']
-        self.nsamples = self.injection.recovery_options['nsamples']
+        self.ntemps = self.injection.recovery_options["ntemps"]
+        self.nsamples = self.injection.recovery_options["nsamples"]
 
     def __call__(self, params):
         """Generate likelihood evaluation
@@ -35,7 +45,7 @@ class LnLikelihoodWvfm():
         ----------
         params: dict
             dictionary of CBC parameters for template
-        
+
         """
         try:
             mchirp = params[0]
@@ -43,67 +53,44 @@ class LnLikelihoodWvfm():
             mass_1, mass_2 = mass1_mass2_from_chirpmass_eta(mchirp, eta)
             spin1z = params[2]
             spin2z = params[3]
+            dist = params[4]
+            dt = params[5]
+            phase = params[6]
         except Exception:
             return -np.inf
 
         template_params = self.injection.injection_parameters.copy()
-        template_params['mass_1'] = mass_1
-        template_params['mass_2'] = mass_2
-        template_params['chi_1'] = spin1z
-        template_params['chi_2'] = spin2z
+        template_params["mass_1"] = mass_1
+        template_params["mass_2"] = mass_2
+        template_params["chi_1"] = spin1z
+        template_params["chi_2"] = spin2z
+        template_params["luminosity_distance"] = dist
 
-        template_polarizations = self.recovery_waveform_generator.frequency_domain_strain(template_params)
-        template_response = self.ifo.get_detector_response(template_polarizations, template_params)[self.frequency_mask]
+        template_polarizations = (
+            self.recovery_waveform_generator.frequency_domain_strain(template_params)
+        )
+        template_response = self.ifo.get_detector_response(
+            template_polarizations, template_params
+        )[self.frequency_mask]
 
-        result = get_overlap(template_response, self.signal_response, self.psd, self.delta_f)
+        rotation = np.exp(-1j * (2.0 * np.pi * self.f * dt + phase))
+
+        result = get_overlap(
+            template_response * rotation, self.signal_response, self.f, self.psd
+        )
+
+        # result = match(template_response, self.signal_response, self.psd, 2**18, True)
+
+        # result = 100 * result ** 4
 
         if np.isfinite(result):
             return result
         else:
             return -np.inf
-        
-def overlap(h1, h2, psd, delta_f):
-    """Computes noise-weighted inner product
 
-    Parameters
-    ----------
-    h1: complex numpy array
-        complex array of frequency domain strain
-    h2: complex numpy array
-        complex array of frequency domain strain
-    psd: numpy array
-        power spectral density of detector
-    delta_f: float
-        frequency spacing of data
-    """
-    integrand = np.dot(h1, h2.conj() / psd)
-    return 4.0 * delta_f * integrand.real
-
-def get_overlap(template, signal, psd, delta_f):
-    """
-    Wrapper to get the loglikelihood from the noise-weighted inner product between 
-    signal and model.
-
-    Parameters
-    ----------
-    template: numpy array
-        complex array of frequency domain template detector response
-    signal: numpy array
-        complex array of frequency domain signal detector response
-    psd: numpy array
-        power spectral density of detector
-    delta_f: float
-        frequency spacing
-    """
-
-    overlap_sig_temp = overlap(template, signal, psd, delta_f)
-    overlap_sig_sig = overlap(signal, signal, psd, delta_f)
-    overlap_temp_temp = overlap(template, template, psd, delta_f)
-
-    return overlap_sig_temp - 0.5 * ( overlap_sig_sig + overlap_temp_temp )
 
 # uniform prior
-class LnPriorUniformWvfm():
+class LnPriorUniformWvfm:
     def __init__(self, mins, maxes):
         """
         mins: vector of minima on uniform prior
@@ -122,6 +109,44 @@ class LnPriorUniformWvfm():
     def initial_sample(self):
         return self.rng.uniform(self.mins, self.maxes)
 
+
+def overlap_dot(h1_in, h2_in, psd):
+    h1 = np.array(h1_in)
+    h2 = np.array(h2_in)
+
+    return 4.0 * (h1 * np.conj(h2)) / psd
+
+
+def overlap(h1_in, h2_in, f, psd):
+
+    integrand = overlap_dot(h1_in, h2_in, psd)
+    return np.real(simpson(integrand, x=f))
+
+
+def get_overlap(template, signal, f, psd):
+    """
+    Wrapper to get the loglikelihood from the noise-weighted inner product between
+    signal and model.
+
+    Parameters
+    ----------
+    template: numpy array
+        complex array of frequency domain template detector response
+    signal: numpy array
+        complex array of frequency domain signal detector response
+    psd: numpy array
+        power spectral density of detector
+    delta_f: float
+        frequency spacing
+    """
+
+    overlap_sig_temp = overlap(template, signal, f, psd)
+    overlap_sig_sig = overlap(signal, signal, f, psd)
+    overlap_temp_temp = overlap(template, template, f, psd)
+
+    return overlap_sig_temp - 0.5 * (overlap_sig_sig + overlap_temp_temp)
+
+
 def mass1_mass2_from_chirpmass_eta(chirp_mass, eta):
     """Function to produce values of mass 1 and mass 2 from the
     chirp mass and symmetric mass ratio
@@ -133,8 +158,93 @@ def mass1_mass2_from_chirpmass_eta(chirp_mass, eta):
     Returns:
         float, float: returns mass1 and mass2, mass1 >= mass2
     """
-    total_mass = chirp_mass / eta**(3.0/5.0)
-    mass_1 = total_mass * (1.0 + (1.0 - 4.0 * eta)**0.5) / 2.0
-    mass_2 = total_mass * (1.0 - (1.0 - 4.0 * eta)**0.5) / 2.0
+    total_mass = chirp_mass / eta ** (3.0 / 5.0)
+    mass_1 = total_mass * (1.0 + (1.0 - 4.0 * eta) ** 0.5) / 2.0
+    mass_2 = total_mass * (1.0 - (1.0 - 4.0 * eta) ** 0.5) / 2.0
 
     return mass_1, mass_2
+
+
+def pad_to_pow_2(arr, zpf, zpf_as_target_length=False):
+    """Pad an array pseudo-evenly on each side
+
+    From https://github.com/Cyberface/phenom/blob/1822f55b2631fad913301b5e7cf0c5b52e31f828/phenom/utils/utils.py
+
+    Parameters
+    ----------
+    arr : np.Array
+        Array to pad
+    zpf : integer
+        Zero padding factor. If 0 then returns original length of
+        arr, rounded up to nearest power of 2.
+
+    Returns
+    -------
+    np.Array
+        The input array zero padded
+    """
+    n = len(arr)
+
+    if zpf_as_target_length and zpf > n:
+        initial_pad_left = int(np.floor((zpf - n) / 2))
+        initial_pad_right = int(np.floor((zpf - n) / 2))
+    else:
+        initial_pad_left = zpf * n
+        initial_pad_right = zpf * n
+    initial_finial_length = initial_pad_left + n + initial_pad_right
+
+    next_power_2 = ceilpow2(initial_finial_length)
+
+    to_add = np.absolute(next_power_2 - initial_finial_length)
+
+    # If even, add to both sides equally
+    if (to_add % 2) == 0:
+        to_add_left = int(to_add / 2)
+        to_add_right = int(to_add / 2)
+    # If odd, add extra one to left side
+    else:
+        to_add_left = int(to_add / 2 + 1)
+        to_add_right = int(to_add / 2)
+
+    left = np.zeros(initial_pad_left + to_add_left)
+    right = np.zeros(initial_pad_right + to_add_right)
+
+    return np.concatenate([left, arr, right])
+
+
+def match(h_1, h_2, psd, zpf=3, zpf_as_target_length=False):
+    """Calculate the match between two waveforms
+
+    Parameters
+    ----------
+    h_1, h_2: np.Array
+        Two waveforms to calculate the match for.
+    psd: np.Array
+        PSD for the match
+    zpf : integer
+        Zero padding factor. If 0 then returns original length of
+        arr, rounded up to nearest power of 2. Increase above zero
+        effectively improves the numerical time shift optimisation
+        performed in the match
+
+    Returns
+    -------
+    float
+        Match between h_1 and h_2 weighted by psd
+    """
+    # Calculate waveform amplitudes
+    A_1 = np.abs(h_1)
+    A_2 = np.abs(h_2)
+
+    # Calculate waveform norms wrt psd
+    norm_1 = np.dot(A_1, A_1 / psd)
+    norm_2 = np.dot(A_2, A_2 / psd)
+
+    # Calculate match integrand
+    integrand = h_1 * h_2.conj() / psd
+    integrand_zero_padded = pad_to_pow_2(integrand, zpf, zpf_as_target_length)
+
+    # Calculate complex SNR sequence to enable time shift optimisation
+    complex_snr = np.asarray(pyfftw.interfaces.numpy_fft.fft(integrand_zero_padded))
+
+    return np.max(np.abs(complex_snr)) / np.sqrt(norm_1 * norm_2)
